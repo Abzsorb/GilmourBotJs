760f992b70b71e45f0e9765ee54ddae99d5c63b3
unknown <Abzsorb@gmail.com>
2020-07-07 17:50:04 +0200

    Added Require Path

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { CommandoClient } = require('discord.js-commando');
-
+const path = require('path')
 const client = new CommandoClient({
     commandPrefix: process.env.prefix,
     owner: process.env.ownerId, 
