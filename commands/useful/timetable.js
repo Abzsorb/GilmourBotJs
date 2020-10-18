4e6a9dcaff9c9d0dfb6c234caabaf4316f1375e6
Abzsorb <44256837+Abzsorb@users.noreply.github.com>
2020-10-18 21:16:16 +0200

    Added another Class

--- commands/useful/timetable.js
+++ commands/useful/timetable.js
@@ -6,11 +6,11 @@ module.exports = class RPS extends commando.Command{
             name:'timetable',
             group:'useful',
             memberName:'timetable',
-            description:'Sends Timetable Choices: S4.2A, M4.2A, M4.2C, M6.1B',
+            description:'Sends Timetable Choices: 3.1E, S4.2A, M4.2A, M4.2C, M6.1B',
             args:[
                 {
                     key: 'text',
-                    prompt: 'Pick your timetable: S4.2A, M4.2A, M4.2C, M6.1B',
+                    prompt: 'Pick your timetable: 3.1E, S4.2A, M4.2A, M4.2C, M6.1B',
                     type: 'string',
                 }
             ]
@@ -27,8 +27,11 @@ module.exports = class RPS extends commando.Command{
         else if (text == "m4.2c"){
             message.channel.send({files:["https://i.imgur.com/2bNBGed.png"]})
         }
-        else if(text = "m6.1b"){
+        else if(text == "m6.1b"){
             message.channel.send({files:["https://i.imgur.com/nG7wn0s.png"]})
+        }
+        else if(text == "3.1e"){
+            message.channel.send({files:["https://i.imgur.com/yhlWDvM.png"]})
         }
     }    
 }
