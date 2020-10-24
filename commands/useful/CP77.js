d7757060b4ec2b31d71266ba4a2c2bb01fc191cb
Abzsorb <44256837+Abzsorb@users.noreply.github.com>
2020-10-24 14:46:52 +0200

    Updated Date

--- commands/useful/CP77.js
+++ commands/useful/CP77.js
@@ -11,7 +11,7 @@ module.exports = class Dice extends commando.Command{
         })
     }
     async run(message){
-        const days =  countdown(new Date(), new Date(2020,10,18), countdown.DAYS).toString()
+        const days =  countdown(new Date(), new Date(2020,10,19), countdown.DAYS).toString()
         message.reply(`Cyberpunk will release in ${days}`)
     }
 }
