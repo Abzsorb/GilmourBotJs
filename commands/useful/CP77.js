c91f3bcf25e36a724219a34945bb5b2c7acb1344
Tyrone Camilleri <tyrone.cmailleri.C10388@mcast.edu.mt>
2020-11-21 07:53:02 +0100

    Updated Countdown

--- commands/useful/CP77.js
+++ commands/useful/CP77.js
@@ -11,8 +11,7 @@ module.exports = class Dice extends commando.Command{
         })
     }
     async run(message){
-        const months = countdown(new Date(), new Date(2020, 11, 10), countdown.MONTHS | countdown.DAYS).toString()
-        const days =  countdown(new Date(), new Date(2020,11,10), countdown.DAYS).toString()
-        message.reply(`Cyberpunk will release in ${months} (${days})`)
+        const time = countdown(new Date(), new Date(2020, 11, 10), countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.MILLISECONDS ).toString()
+        message.reply(`Cyberpunk will release in ${time}`)
     }
 }
