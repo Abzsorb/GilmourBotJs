5bc0f0c3d12889a842dfab20b5b214035f97eff6
Tyrone Camilleri <tyrone.cmailleri.C10388@mcast.edu.mt>
2020-12-07 09:26:54 +0100

    Fixed Time?

--- commands/useful/CP77.js
+++ commands/useful/CP77.js
@@ -1,17 +1,30 @@
-const commando = require("discord.js-commando")
-const countdown = require("./countdown.js")
+const commando = require("discord.js-commando");
+const countdown = require("./countdown.js");
 
-module.exports = class Dice extends commando.Command{
-    constructor(client){
-        super(client,{
-            name:'cp77',
-            group:'useful',
-            memberName:'cp77',
-            description:'Gives you how much time is left until Cyberpunk 2077 to come out'
-        })
-    }
-    async run(message){
-        const time = countdown(new Date(), new Date(2020, 11, 10, 1), countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.MILLISECONDS ).toString()
-        message.reply(`Cyberpunk will release in ${time}`)
-    }
-}
+module.exports = class Dice extends (
+  commando.Command
+) {
+  constructor(client) {
+    super(client, {
+      name: "cp77",
+      group: "useful",
+      memberName: "cp77",
+      description:
+        "Gives you how much time is left until Cyberpunk 2077 to come out",
+    });
+  }
+  async run(message) {
+    var dateFuture = new Date(2020,11,10,1);
+    var dateNow = new Date();
+
+    var seconds = Math.floor((dateFuture - dateNow) / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+
+    hours = hours - days * 24;
+    minutes = minutes - days * 24 * 60 - hours * 60;
+    seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+    message.replay(`Cyberpunk will release in ${hours} hours ${minutes} minutes ${seconds} seconds`)
+  }
+};
